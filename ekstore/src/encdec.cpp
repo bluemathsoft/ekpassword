bade314cadbf162a4084a8abb598b166d66e3f04
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-08-04 08:28:13 -0400

    Use variable plain text

--- ekstore/src/encdec.cpp
+++ ekstore/src/encdec.cpp
@@ -4,18 +4,18 @@ Copyright (C) 2017 Jayesh Salvi, Blue Math Software Inc.
 
 This file is part of ekpassword.
 
-bluemath is free software: you can redistribute it and/or modify
+ekpassword is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-bluemath is distributed in the hope that it will be useful,
+ekpassword is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with bluemath. If not, see <http://www.gnu.org/licenses/>.
+along with ekpassword. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
--- ekstore/src/main.cpp
+++ ekstore/src/main.cpp
@@ -4,60 +4,18 @@ Copyright (C) 2017 Jayesh Salvi, Blue Math Software Inc.
 
 This file is part of ekpassword.
 
-bluemath is free software: you can redistribute it and/or modify
+ekpassword is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-bluemath is distributed in the hope that it will be useful,
+ekpassword is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with bluemath. If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-/*
-#include <iostream>
-#include <string>
-#include <openssl/evp.h>
-#include <openssl/aes.h>
-
-void encrypt(
-  const char *indata,
-  size_t indata_len,
-  char *outdata,
-  int *outLen2)
-{
-  int outLen1 = 0;
-  unsigned char ckey[] =  "password";
-  unsigned char ivec[] = "donotchangethis!";
-
-  //Set up encryption
-  EVP_CIPHER_CTX ctx;
-  EVP_EncryptInit(&ctx,EVP_aes_256_cbc(),ckey,ivec);
-  EVP_EncryptUpdate(&ctx,outdata,&outLen1,indata,indata_len);
-  std::cout << "outlen1 " << outLen1 << std::endl;
-  EVP_EncryptFinal(&ctx,outdata + outLen1,outLen2);
-}
-
-int main(int argc, char *argv[])
-{
-
-  std::string msg("This is my secret text");
-
-  char *encmsg = (char*)malloc(msg.length() * 2);
-  int encmsglen;
-
-  encrypt(msg.c_str(), msg.length(), encmsg, &encmsglen);
-
-  std::cout << msg.length() << std::endl;
-  std::cout << encmsglen << std::endl;
-
-  return 0;
-}
+along with ekpassword. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -65,6 +23,7 @@ int main(int argc, char *argv[])
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <string.h>
+#include <sstream>
 #include "encdec.h"
 
 int main (void)
@@ -79,18 +38,22 @@ int main (void)
   /* A 128 bit IV */
   unsigned char *iv = (unsigned char *)"0123456789012345";
 
-  /* Message to be encrypted */
-  unsigned char *plaintext =
-                (unsigned char *)"The quick brown fox jumps over the lazy dog";
 
-  /* Buffer for ciphertext. Ensure the buffer is long enough for the
-   * ciphertext which may be longer than the plaintext, dependant on the
-   * algorithm and mode
-   */
-  unsigned char ciphertext[128];
+  std::stringstream plainss;
+  plainss << "[";
+  plainss << "  {";
+  plainss << "    \"url\" : \"https://google.com\",";
+  plainss << "    \"password\" : \"12345\"";
+  plainss << "  }";
+  plainss << "]";
 
-  /* Buffer for the decrypted text */
-  unsigned char decryptedtext[128];
+  std::string plainstr = plainss.str();
+
+  unsigned char *plaintext = (unsigned char*) plainstr.c_str();
+
+  unsigned char *ciphertext = new unsigned char[2*plainstr.size()];
+
+  unsigned char *decryptedtext = new unsigned char[2*plainstr.size()];
 
   int decryptedtext_len, ciphertext_len;
 
